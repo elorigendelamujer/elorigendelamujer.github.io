519e09f93cea4cb384cb467f36f786d0f3775fea
elorigendelamujer <83359509+elorigendelamujer@users.noreply.github.com>
2021-04-30 21:20:32 +0200

    Títulos añadidos al mapa, primera pieza en un contenedor que se puede visualizar, transiciones

--- script/index.js
+++ script/index.js
@@ -1,8 +1,17 @@
 function alEntrar() {
-  document.getElementById("entrada").style.display = "none";
+  document.getElementById("entrada").style.opacity = "0";
+  nuevaPagina();
+  setTimeout(ocultarEntrada, 1000);
+}
+
+function nuevaPagina() {
   document.getElementById("principal").style.display = "block";
   if (screen.width > 1000) {
-    document.body.style.background = "black url('img/fondo-web.jpeg') no-repeat fixed center center";
+    document.body.style.background = "url('img/fondo-web.jpeg') black no-repeat fixed center center";
     document.body.style.backgroundSize = "cover";
   }
+}
+
+function ocultarEntrada() {
+  document.getElementById("entrada").style.display = "none";
 }
--- script/principal.js
+++ script/principal.js
@@ -1,3 +1,25 @@
-function mostrarLucy() {
-  document.getElementById("img01").style.display = "block";
+function mostrar(id) {
+  var img = "img" + id;
+  var container = "container-img" + id;
+  document.getElementById(img).style.display = "inline-block";
+  document.getElementById(container).style.display = "block";
+  document.getElementById(container).style.transition = "0.5s";
+  ocultarMap();
+  document.getElementById(container).style.opacity = "1";
+}
+
+function ocultarMap() {
+  document.getElementById("map").style.display = "none";
+}
+
+function mostrarMap() {
+  document.getElementById("map").style.display = "block";
+}
+
+function cerrarImagen(id) {
+  document.getElementById(id).style.opacity = "0";
+  setTimeout(function() {
+    document.getElementById(id).style.display = "none";
+  }, 500);
+  mostrarMap();
 }
